c1410314908692d5ada3497664d6819d5ba39966
Jeremy Seago <seagoj@gmail.com>
2013-04-24 10:33:59 -0500

    Creates hookTest

---
+++ tests/hookTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class HookTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $_REQUEST['payload'] = "test";
+    }
+
+    public function tearDown()
+    {
+        unset($_REQUEST)
+    }
+
+    public function testHook()
+    {
+        $this->assertEquals($REQUEST['payload'], "test");
+//        $hook = new Hook();
+    }
+}
+
