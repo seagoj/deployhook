ef516de3ebeb0383563e996cf6ab810d185a29be
Jeremy Seago <seagoj@gmail.com>
2013-04-25 17:01:06 -0500

    Adds defaults back in

--- tests/hookTest.php
+++ tests/hookTest.php
@@ -4,10 +4,14 @@ class HookTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
+        $_REQUEST['payload'] = json_decode(file_get_content('tests/payload.json'));
+        $_SERVER['HTTP_CLIENT_IP'] = '127.0.0.1';
     }
 
     public function tearDown()
     {
+        unset($_REQUEST);
+        unset($_SERVER['HTTP_CLIENT_IP']));
     }
 
     public function testPayload()
