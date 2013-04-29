45f057bdb5a7d537efb16d0c75fe8af1a197759c
Jeremy Seago <seagoj@gmail.com>
2013-04-29 14:43:47 -0500

    Makes stepdown docroot in HookTest.setup

--- tests/hookTest.php
+++ tests/hookTest.php
@@ -6,7 +6,9 @@ class HookTest extends PHPUnit_Framework_TestCase
     {
         $_REQUEST['payload'] = file_get_contents('tests/payload.json');
         $_SERVER['HTTP_CLIENT_IP'] = '127.0.0.1';
-        mkdir('/var/www/hook');
+        if(!is_dir('/var')) mkdir('/var');
+        if(!is_dir('/var/www')) mkdir('/var/www');
+        if(!is_dir('/var/www/hook')) mkdir('/var/www/hook');
     }
 
     public function tearDown()
