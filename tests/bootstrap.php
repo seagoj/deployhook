518ef548be5ae5ff1a0b90f2d47d6f1e3eca4c2f
Jeremy Seago <seagoj@gmail.com>
2013-04-29 17:14:28 -0500

    Syntax error

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ function loader($class)
 {
     $file = $class . '.php';
     if (file_exists($file)) {
-        require '$file;
+        require $file;
     } else if (file_exists('lib/Devtools/'.$file)) {
         require 'lib/Devtools/'.$file;        
     }
